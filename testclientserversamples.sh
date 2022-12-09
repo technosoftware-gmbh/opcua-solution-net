e19894fb74c84be000af075bba7f6bbaf23726eb
Technosoftware GmbH <support@technosoftware.com>
2022-12-09 12:20:53 +0100

    Updated test scripts

--- testclientserversamples.sh
+++ testclientserversamples.sh
@@ -1,103 +1,61 @@
 #!/bin/bash
-echo Test the .NET Core Server and .NET Core Client
+echo Test .NET 6.0 Reference Server and Refererence Client
 workdir=$(pwd)
-sampleclientresult=0
-sampleclientreverseresult=0
-sampleclienthttpsresult=0
-sampleserverresult=0
-cd examples/Simulation/SampleServer
-echo build SampleServer
+referenceclientresult=0
+referenceserverresult=0
+cd applications/SampleCompany/ReferenceServer
+echo build ReferenceServer
 rm -r obj
-dotnet build SampleCompany.SampleServer.csproj
-cd ../SampleClient
-echo build SampleClient
+dotnet build SampleCompany.ReferenceServer.csproj -f net6.0
+cd ../ReferenceClient
+echo build ReferenceClient
 rm -r obj
-dotnet build SampleCompany.SampleClient.csproj
+dotnet build SampleCompany.ReferenceClient.csproj -f net6.0
 cd "$workdir"
 
-cd examples/Simulation/SampleServer
-echo start SampleServer with reverse connection enabled
-touch ./SampleServer.log
-#dotnet run --no-restore --no-build --project SampleCompany.SampleServer.csproj -- -t 120 -a -rc=opc.tcp://localhost:55555 >./SampleServer.log &
-dotnet run --no-restore --no-build --project SampleCompany.SampleServer.csproj -- -t 120 -a >./SampleServer.log &
-sampleserverpid="$!"
-echo wait for SampleServer to be started
-grep -m 1 "start" <(tail -f ./SampleServer.log --pid=$sampleserverpid)
-tail -f ./SampleServer.log --pid=$sampleserverpid &
+cd applications/SampleCompany/ReferenceServer
+echo start ReferenceServer
+touch ./ReferenceServer.log
+dotnet run --framework net6.0 --no-restore --no-build --project SampleCompany.ReferenceServer.csproj -- -t 120 -a >./ReferenceServer.log &
+referenceserverpid="$!"
+echo wait for ReferenceServer to be started
+grep -m 1 "start" <(tail -f ./ReferenceServer.log --pid=$referenceserverpid)
+tail -f ./ReferenceServer.log --pid=$referenceserverpid &
 cd "$workdir"
 
-#cd examples/Simulation/SampleClient
-#echo start SampleClient for tcp connection with reverse connection
-#dotnet run --no-restore --no-build --project SampleCompany.SampleClient.csproj -- -t 40 -a -rc=opc.tcp://localhost:55555 &
-#sampleclientreversepid="$!"
-#cd "$workdir"
-
-cd examples/Simulation/SampleClient
-echo start SampleClient for tcp connection
-dotnet run --no-restore --no-build --project SampleCompany.SampleClient.csproj -- -t 20 -a &
-sampleclientpid="$!"
+cd applications/SampleCompany/ReferenceClient
+echo start ReferenceClient for tcp connection
+dotnet run --framework net6.0 --no-restore --no-build --project SampleCompany.ReferenceClient.csproj -- -t 20 -a &
+referenceclientpid="$!"
 cd "$workdir"
 
-#cd examples/Simulation/SampleClient
-#echo start SampleClient for https connection
-#dotnet run --no-restore --no-build --project SampleCompany.SampleClient.csproj -- -t 20 -a https://localhost:55551 &
-#httpsclientpid="$!"
-#cd "$workdir"
-
-echo wait for SampleClient
-wait $sampleclientpid
+echo wait for ReferenceClient
+wait $referenceclientpid
 if [ $? -eq 0 ]; then
-	echo "SUCCESS - SampleClient test passed"
+	echo "SUCCESS - ReferenceClient test passed"
 else
-	sampleclientresult=$?
-	echo "FAILED - SampleClient test failed with a status of $sampleclientresult"
+	referenceclientresult=$?
+	echo "FAILED - ReferenceClient test failed with a status of $referenceclientresult"
 fi
 
-cd examples/Simulation/SampleClient
 cd "$workdir"
 
-#echo wait for SampleClient with reverse connection
-#wait $sampleclientreversepid
-#if [ $? -eq 0 ]; then
-#	echo "SUCCESS - SampleClient with reverse connection test passed"
-#else
-#	sampleclientreverseresult=$?
-#	echo "FAILED - SimpleClient with reverse connection test failed with a status of $sampleclientreverseresult"
-#fi
+echo send Ctrl-C to ReferenceServer
+kill -s SIGINT $referenceserverpid
 
-#cd examples/Simulation/SampleClient
-#cd "$workdir"
-
-#echo wait for SampleClient with https connection
-#wait $httpsclientpid
-#if [ $? -eq 0 ]; then
-#	echo "SUCCESS - SampleClient with https connection test passed"
-#else
-#	sampleclienthttpsresult=$?
-#	echo "FAILED - Client test failed with a status of $sampleclienthttpsresult"
-#	echo "FAILED - Client may require to use trusted TLS server cert to pass this test"
-#	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
-#		sampleclienthttpsresult=0 
-#		echo "IGNORED - test requires trusted TLS cert on OSX"
-#	fi
-#fi
-
-echo send Ctrl-C to SampleServer
-kill -s SIGINT $sampleserverpid
-
-echo wait for SampleServer
-wait $sampleserverpid
-sampleserverresult=$?
+echo wait for ReferenceServer
+wait $referenceserverpid
+referenceserverresult=$?
 
 if [ $? -eq 0 ]; then
-	echo "SUCCESS - SampleServer test passed"
+	echo "SUCCESS - ReferenceServer test passed"
 else
-	sampleserverresult=$?
-	echo "FAILED - SampleServer test failed with a status of $sampleserverresult"
+	referenceserverresult=$?
+	echo "FAILED - ReferenceServer test failed with a status of $referenceserverresult"
 fi
 
-echo "Test results: SampleClient:$sampleclientresult SampleClientReverse:$sampleclientreverseresult SampleClienthttps:$sampleclienthttpsresult SampleServer:$sampleserverresult"
-exit $((sampleclientresult + sampleclientreverseresult + sampleclienthttpsresult + sampleserverresult))
+echo "Test results: ReferenceClient:$referenceclientresult ReferenceServer:$referenceserverresult"
+exit $((referenceclientresult + referenceserverresult))
 
 
 
